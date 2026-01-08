a13c3e3a4ef19e26bfe334d12eb59c30a905bc02
Alexandre Bencz <alebencz@gmail.com>
2026-01-08 16:27:50 -0300

    REMOVED MASM

---
+++ samples/SimpleZero.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Before");
+        
+        // Teste: Int32ToString com valor 0 (caso especial que retorna "0" literal)
+        int x = 0;
+        Console.WriteLine(x);
+        
+        Console.WriteLine("After");
+    }
+}
